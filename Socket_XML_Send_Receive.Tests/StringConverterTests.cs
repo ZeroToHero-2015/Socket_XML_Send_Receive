042981f72c827f3597efa7b43dab2daca03f4644
Victor Grigoriu <victorg@teamnet.ro>
2016-07-15 10:33:45 +0300

    Add tests for StringConverter

---
+++ Socket_XML_Send_Receive.Tests/StringConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Socket_XML_Send_Receive.Tests
+{
+    [TestFixture]
+    public class StringConverterTests
+    {
+        [Test]
+        public void ConvertsToASCIIWithoutPrefix()
+        {
+            var result = StringConverter.GetBytesToSend("ASCII","asds",false);
+            byte[] array = { 97, 115, 100, 115 };
+            Assert.That(result, Is.EqualTo(array));
+        }
+    }
+}
